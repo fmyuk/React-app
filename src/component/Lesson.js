99a9347870cec0435a5873325f59198b0323dbf5
fumi uk <dcfumiya.238@gmail.com>
2020-09-11 10:59:42 +0900

    react-app

---
+++ src/component/Lesson.js
@@ -0,0 +1,16 @@
+import React from "/react";
+
+class Lesson extends React.Component {
+  render() {
+    return (
+      <div className="lesson-card">
+        <div className="lesson-item">
+          <a></a>
+          <img />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Lesson;
